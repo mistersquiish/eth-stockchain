744d6fe895762c840ad820094abbd9d5c6b01dad
mistersquiish <henryvuong@utexas.edu>
2020-02-15 11:39:23 +0100

    server works

--- contracts/CompanyListing.sol
+++ contracts/CompanyListing.sol
@@ -1,18 +1,32 @@
 pragma solidity ^0.5.0;
 
 contract CompanyListing {
-	uint public listingCount;
+	uint public listingCount = 0;
 
-	// struct Listing {
-	// 	uint id;
+	struct Listing {
+		uint id;
+		bool approved;
 
-	// 	// basic company information
-	// 	string companyName;
-	// 	string address;
-	// 	string country;
+		// basic company information
+		string companyName;
+		// string address;
+		// string country;
 
-	// 	// basic company financials
-		
+		// basic company financials
 
-	// }
+		// basic IPO information
+		// uint stockPrice;
+		// uint sharesNum;
+	}
+
+	mapping(uint => Listing) public listings;
+
+	constructor() public {
+		createListing("Google");
+	}
+
+	function createListing(string memory _companyName) public {
+		listingCount ++;
+		listings[listingCount] = Listing(listingCount, false, _companyName);
+	}
 }
